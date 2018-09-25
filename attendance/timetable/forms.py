28d91b9dadb44f8f584ddfa856d34f6fed4d0699
gizolka <gizjar@icloud.com>
2018-09-25 15:56:29 +0200

    Created forms.py and playing around with it

--- attendance/timetable/forms.py
+++ attendance/timetable/forms.py
@@ -2,8 +2,7 @@
 from django import forms
 from .models import Event
 
-class EventForm(formsModelForm):
-
-    class Meta:
+class EventForm(forms.ModelForm):
+  class Meta:
         model = Event
-        fields = ('title','type_of_event', 'date', 'end_date', 'description')
+        fields = ['title','type_of_event', 'date', 'end_date', 'description']
--- attendance/timetable/views.py
+++ attendance/timetable/views.py
@@ -2,13 +2,32 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.template import loader
 from .models import Event
 
-def home(request):
+from .forms import EventForm
+
+def new_event(request):
+
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/thanks/')
+
+        else:
+            form = EventForm()
+
+        return render(request, 'event.html', {'form': form})
+
+
+def index(request):
     query_request = Event.objects.all()
-    template = loader.get_template('timetable/home.html')
+    template = loader.get_template('timetable/index.html')
     context = {
         'query_request': query_request,
     }
     return HttpResponse(template.render(context, request))
+
+def links(request):
+    return render(request, 'timetable/links.html', {})
